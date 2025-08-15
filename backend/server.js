581489479c9a5b3cc52478a12a417f2f04add547
Hamza <hamzaflash44@gmail.com>
2025-08-15 15:32:39 +1000

    PDDTA-2: test

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,19 @@
-require('dotenv').config();
+require('dotenv').config();        
 
 const express = require('express');
-const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
-dotenv.config();
-
-
 const app = express();
+
+if (process.env.NODE_ENV !== 'test') {
+  connectDB();                     
+}
 
 app.use(cors());
 app.use(express.json());
-app.use('/api/auth', require('./routes/authRoutes'));
 
+app.use('/api/auth', require('./routes/authRoutes'));
 app.use('/api/tasks', require('./routes/taskRoutes'));
 
 // Error handling middleware
