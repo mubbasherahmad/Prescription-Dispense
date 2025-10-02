a4e9f85972020e335d0d5149ea93cf926a6e42a8
Muhammad Mubbashar <mubbasharahmad001@gmmail.com>
2025-10-02 21:23:13 +0500

    Github Workflow CHeck#4

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://52.64.174.115:5001', // local
   // baseURL: 'http://13.55.31.24:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
